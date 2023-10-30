28b66dd75fe5314e264bb3c6f0f2c74deb6da5c6
c-wrenn <crislynwren@yahoo.com>
2023-10-30 02:14:29 -0500

    "associations created"

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -3,15 +3,18 @@ const Product = require('./Product');
 const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
+//create associations
+//include foreign keys
+//add a onDelete to all associations
 
 // Products belongsTo Category
-
+Product.belongsTo(Category, {foreignKey: 'category_id', onDelete: 'CASCADE'});
 // Categories have many Products
-
+Category.hasMany(Product, {foreignKey: 'category_id', onDelete: 'CASCADE'});
 // Products belongToMany Tags (through ProductTag)
-
+Product.belongsToMany(Tag, {through: ProductTag, foreignKey: 'product_id', onDelete: 'CASCADE'});
 // Tags belongToMany Products (through ProductTag)
-
+Tag.belongsToMany(Product, {through: ProductTag, foreignKey:'tag_id', onDelete: 'CASCADE'});
 module.exports = {
   Product,
   Category,
